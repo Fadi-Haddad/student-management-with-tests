09f3b5fe3515b3e5f7632757d30eb0ef8062e3bc
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-10-06 17:10:46 +0300

    Backend::seedling:Add Code to Seed Users Table

--- student-managment-backend/database/seeders/UsersTableSeeder.php
+++ student-managment-backend/database/seeders/UsersTableSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        DB::table('users')->insert([
+            'name' => 'firstStudent',
+            'email' => 'firststudent@domain.com',
+            'password' => Hash::make('firstStudentPass'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'secondStudent',
+            'email' => 'secondstudent@domain.com',
+            'password' => Hash::make('secondStudentPass'),
+        ]);
     }
 }
+
+
