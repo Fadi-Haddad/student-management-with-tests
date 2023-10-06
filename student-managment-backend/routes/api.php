45fe5b7530794a361127efb3e1e81f3a5e3c738d
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-10-06 12:52:00 +0300

    Backend: Add Route to Create a New Student

--- student-managment-backend/routes/api.php
+++ student-managment-backend/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/students', 'StudentController@getAllStudents');
+Route::get('/create-new-user', 'StudentController@createNewUser');
