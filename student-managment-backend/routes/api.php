38f17a26879f867cccb44cf38904e20e5102d311
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-10-07 01:15:06 +0300

    Backend: Fix Route Definition in web.php

--- student-managment-backend/routes/api.php
+++ student-managment-backend/routes/api.php
@@ -2,28 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/students', 'StudentController@getAllStudents');
-Route::get('/create-new-user', 'StudentController@createNewStudent');
-Route::get('/students/{id}', 'StudentController@getStudentByID');
-Route::delete('/students/{id}', 'StudentController@deleteStudentByID');
 
-Route::get('/courses', 'StudentController@getAllCourses');
-Route::get('/create-new-course', 'StudentController@createNewCourse');
-Route::get('/courses/{id}', 'StudentController@getCourseByID');
-Route::delete('/courses/{id}', 'StudentController@deleteCourseByID');
+Route::get('/courses', [CourseController::class, 'getAllCourses']);
+Route::get('/create-new-course', [CourseController::class, 'createNewCourse']);
+Route::get('/courses/{id}', [CourseController::class, 'getCourseByID']);
+Route::delete('/courses/{id}', [CourseController::class, 'deleteCourseByID']);
+
+
+Route::get('/students', [StudentController::class, 'getAllStudents']);
+Route::get('/create-new-student', [StudentController::class, 'createNewStudent']);
+Route::get('/students/{id}', [StudentController::class, 'getStudentByID']);
+Route::delete('/students/{id}', [StudentController::class, 'deleteStudentByID']);
