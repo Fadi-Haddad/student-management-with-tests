249612895587354dd2734b489437da760da12a39
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-10-06 20:17:53 +0300

    Backend: Create displayStudents Function in Student Web Controller

--- student-managment-backend/app/Http/Controllers/StudentWebController.php
+++ student-managment-backend/app/Http/Controllers/StudentWebController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+
 use Illuminate\Http\Request;
 
 class StudentWebController extends Controller
 {
-    //
+    public function displayStudents(){
+
+        $students = Student::all();
+
+        return view('students.displayall', ['students' => $students]);
+    }
 }
