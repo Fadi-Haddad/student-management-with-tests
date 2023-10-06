f11248a98fceddc9d3cfef25cd73532fd61d08d7
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-10-06 12:34:30 +0300

    Backend: Add getAllStudents  Function to Student Controller to Retreive All Students

--- student-managment-backend/app/Http/Controllers/StudentController.php
+++ student-managment-backend/app/Http/Controllers/StudentController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function getAllStudents() {
+
+        $students = Student::select('id', 'name', 'age')->get();
+
+        return response()->json(['students'=>$students]);
+    }
 }
