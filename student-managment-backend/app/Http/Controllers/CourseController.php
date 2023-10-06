5b37a4bed20dba9727a386f3f8aac9ccd86b02f5
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-10-06 13:14:00 +0300

    Backend: Add getAllCourses Function to Course Controller

--- student-managment-backend/app/Http/Controllers/CourseController.php
+++ student-managment-backend/app/Http/Controllers/CourseController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
-    //
+    public function getAllCourses() {
+        $courses = Course::all();
+
+        return response()->json(['Courses'=>$courses]);
+
+    }
 }
